f8d0bc173df81a995b862b36c5913c54a8c12a94
Abhishek Prasad <devcula@outlook.com>
2023-11-18 13:25:18 +0530

    Added basic structure, packages and router config

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,38 @@
 import express from 'express';
+import * as bodyParser from 'body-parser';
+import cors from 'cors';
+import AuthRouter from './routes/auth.router';
+
+process.on('uncaughtException', (error) => {
+    console.error('[GLOBAL] uncaught exception', error);
+});
+
+process.on('unhandledRejection', (error) => {
+    console.error('[GLOBAL] unhandled rejection', error);
+});
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Add middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send('Working!');
+});
+
+// Define all the routes here and attach the routers.
+const allRoutes = [
+    {
+        path: "/auth",
+        router: AuthRouter
+    }
+];
+
+// Attaching all the defined routes to the server
+allRoutes.forEach(route => {
+    app.use(route.path, route.router);
 });
 
 app.listen(port, () => {
