e93baf60af51043a6436623d4b0f653cb5023d42
Abhishek Prasad <devcula@outlook.com>
2023-12-19 10:56:16 +0530

    file upload location changes

--- config.ts
+++ config.ts
@@ -1,12 +1,12 @@
 const config = {
     JWT_PASSPHRASE: process.env.JWT_PASSPHRASE || 'test-secret',
     MYSQL: {
-        HOST: process.env.MYSQL_HOST || "localhost",
+        HOST: process.env.MYSQL_HOST || "host.docker.internal",
         USER: process.env.MYSQL_USER || "root",
         PASSWORD: process.env.MYSQL_PASSWORD || "root",
         DATABASE: "TEST_DEV_DB"
     },
-    FILES_STORAGE_LOCATION: '/home/devcula/Workspace/fileUploads'
+    FILES_STORAGE_LOCATION: process.env.FILE_PATH || '/tmp/fileUploads'
 };
 
 export default config;
